ecc874b410a6c15b78335865bb3434da54de59ac
AbubakerSiddiq <abubakkarsiddiq1989@gmail.com>
2020-12-30 13:51:21 +0530

    Completed basic flow

--- src/main/java/testNgpratice/TravelSite.java
+++ src/main/java/testNgpratice/TravelSite.java
@@ -21,6 +21,11 @@ public class TravelSite {
 	WebDriver driver;
 	WebDriverWait wait;
 
+	/*
+	 *  setup chromedriver
+	 *  maximize
+	 *  
+	 */
 	@BeforeSuite
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
@@ -30,6 +35,12 @@ public class TravelSite {
 		wait = new WebDriverWait(driver, 30);
 	}
 
+	/*
+	 * passing url
+	 * implicate wait
+	 */
+			
+	
 	@BeforeClass
 	public void launchSite() {
 		driver.get("https://www.phptravels.net/home");
@@ -37,7 +48,9 @@ public class TravelSite {
 	}
 
 	WebElement myAccount, login, userName, password, loginBtn;
-
+/*
+ *login Scenario 
+ */
 	@BeforeMethod
 	public void loginPage() throws InterruptedException {
 		myAccount = driver.findElement(By.cssSelector("div.mini-menu>ul>li:nth-child(3)"));
@@ -49,7 +62,7 @@ public class TravelSite {
 		String currentPageUrl = driver.getCurrentUrl();
 		Thread.sleep(4000);
 		Assert.assertEquals(currentPageUrl, "https://www.phptravels.net/login");
-//Enter username and password
+		//Enter username and password
 		userName = driver.findElement(By.name("username"));
 		password = driver.findElement(By.name("password"));
 		loginBtn = driver.findElement(By.xpath("//button[contains(text(),'Login')]"));
@@ -60,28 +73,43 @@ public class TravelSite {
 		Thread.sleep(3000);
 	}
 
+	/*
+	 * Main test Case add 
+	 * 1. Vaildating username 
+	 * 2. if user name is correct then it will click home
+	 * 3. then validated home url 
+	 */
 	List<WebElement> homeLink;
-
 	@Test
 	public void testCase1() {
 		// validate the user name
 		String usernameText = driver.findElement(By.cssSelector("div.mini-menu>ul>li:nth-child(3)>div>a")).getText();
 		homeLink = driver.findElements(By.cssSelector("div#mobileMenuMain>nav>ul>li>a"));
+		
+		//Click the home link
 		if (usernameText.equals("DEMO")) {
 
 			System.out.println(homeLink.size());
 			homeLink.get(0).click();
 		}
+		//waiting till the url is present and Verified expected and actual result
 		wait.until(ExpectedConditions.urlMatches("https://www.phptravels.net/home"));
 		String homePageUrl = driver.getCurrentUrl();
 		Assert.assertEquals(homePageUrl, "https://www.phptravels.net/home");
 	}
 
-	WebElement logOut;
+	/*
+	 *Logout Scenario
+	 *  1.Assigned webelement globally and stored the element in the for each action
+	 *  2.Verified page is logOut 
+	 */
+	
+	WebElement logOut, clickUserDrp;
 
 	@AfterMethod
-	public void logOut() throws InterruptedException {
-		myAccount.click();
+	public void logOut() throws InterruptedException {	
+		clickUserDrp = driver.findElement(By.cssSelector("div.mini-menu>ul>li:nth-child(3)>div>a"));
+		clickUserDrp.click();
 		logOut = driver.findElement(By.xpath("//a[contains(text(),'Logout')]"));
 		wait.until(ExpectedConditions.visibilityOf(logOut));
 		logOut.click();
